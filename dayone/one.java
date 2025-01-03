eb4244689774a52a4078cfef2e2632731768506b
aryan55254 <aryanmishra55254@gmail.com>
2025-01-03 14:45:06 +0530

    java datatypes and first lines of code

---
+++ dayone/one.java
@@ -0,0 +1,42 @@
+package dayone;
+class one {
+    public static void main(String[] args) {
+        {
+            //int num1 = 3;
+            //int num2 = 4;
+            //int result = num1 + num2;
+            //System.out.println("first lines of code in java");
+            //System.out.println(7+8);
+           // System.out.println(8-5);
+            //System.out.println(num1);
+            //System.out.println(num2);
+            //System.out.println(num1 + num2);
+            //System.out.println(result);
+           //double num = 58.665;
+           //float num2 = 5.65f;
+           //System.out.println(num);
+           //System.out.println(num2);
+           //boolean x = true;
+           //char d= 'g';//d is variable g is literal
+           //char e = '4';
+           //System.out.println(x);
+           //System.out.println(d);
+           //System.out.println(e);
+
+   // char c = 'a';
+   // c++;
+   // System.out.print(c);//prints b
+     
+   char c = '5';
+   c++;
+   System.err.println(c);
+
+                }
+    }
+}
+//data types 
+//primitive datatypes = int , float(decimal values) , character(a,b,c) , boolean(t/f)
+// int = byte(1/8bits) (-127 to 127) , short(2byte) , int(4bytes), long(8bytes)
+//float - 4 bytes , double - 8 bytes(defult)
+//char should be in single quotes
+  
